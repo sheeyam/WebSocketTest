d9d0bbd471008b5f38cc5451e3eab57b3b66b881
Sheeyam <sheeyam.shellvacumar@gmail.com>
2019-02-16 19:52:29 -0600

    Web Socket Test Initial Files

---
+++ WebSocketTest/WebSocketTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using SuperWebSocket;
+
+namespace WebSocketTest
+{
+    class Program
+    {
+        private static WebSocketServer wsServer;
+        static void Main(string[] args)
+        {
+            wsServer = new WebSocketServer();
+            int port = 8088;
+            wsServer.Setup(port);
+            wsServer.NewSessionConnected += WsServer_NewSessionConnected;
+            wsServer.NewMessageReceived += WsServer_NewMessageReceived;
+            wsServer.NewDataReceived += WsServer_NewDataReceived;
+            wsServer.SessionClosed += WsServer_SessionClosed;
+            wsServer.Start();
+            Console.WriteLine("Server is running on port " + port + ". Press ENTER to exit....");
+            Console.ReadKey();
+        }
+
+        private static void WsServer_SessionClosed(WebSocketSession session, SuperSocket.SocketBase.CloseReason value)
+        {
+            Console.WriteLine("Session Closed");
+        }
+
+        private static void WsServer_NewDataReceived(WebSocketSession session, byte[] value)
+        {
+            Console.WriteLine("New Data Received");
+        }
+
+        private static void WsServer_NewMessageReceived(WebSocketSession session, string value)
+        {
+            Console.WriteLine("New Message Received: " + value);
+            if (value == "Hello server")
+            {
+                session.Send("Hello client");
+            }
+        }
+
+        private static void WsServer_NewSessionConnected(WebSocketSession session)
+        {
+            Console.WriteLine("New Session Connected");
+        }
+    }
+}
