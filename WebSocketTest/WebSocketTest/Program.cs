ebf95e8e753fcea27eded5c150e7ac53189b4b21
Sheeyam <sheeyam.shellvacumar@gmail.com>
2019-04-06 01:16:29 -0500

    Latest Web CC Application Code

--- WebSocketTest/WebSocketTest/Program.cs
+++ WebSocketTest/WebSocketTest/Program.cs
@@ -11,8 +11,6 @@ namespace WebSocketTest
     {
         private static WebSocketServer wsServer;
         private static int initialConnPort = 9090;
-        private static int currConnPort = 9090;
-        //private static string hololensIP = "10.91.68.73";
         private static string[] hololensIP;
 
         static void Main(string[] args)
@@ -50,19 +48,6 @@ namespace WebSocketTest
             else {
                 // No Devices
             } 
-
-            /*
-            wsServer = new WebSocketServer();
-            int port = 8088;
-            wsServer.Setup(port);
-            wsServer.NewSessionConnected += WsServer_NewSessionConnected;
-            wsServer.NewMessageReceived += WsServer_NewMessageReceived;
-            wsServer.NewDataReceived += WsServer_NewDataReceived;
-            wsServer.SessionClosed += WsServer_SessionClosed;
-            wsServer.Start();
-            Console.WriteLine("Server is running on port " + port + ". Press ENTER to exit....");
-            Console.ReadKey();
-            */
         }
 
         private static void WsServer_SessionClosed(WebSocketSession session, SuperSocket.SocketBase.CloseReason value)
@@ -79,6 +64,7 @@ namespace WebSocketTest
         {
             Console.WriteLine("New Message Received: " + value);
 
+            /*
             string scene = null;
             string cmd = null;
 
@@ -100,124 +86,8 @@ namespace WebSocketTest
             else {
                 Console.WriteLine("Error: Check Input Size on Web Module" + cmd);
             }
-
-            if (cmd.Equals("CONNN"))
-            {
-                Connect(hololensIP, initialConnPort, scene, cmd);
-
-                switch (scene)
-                {
-                    case "SCN01":
-                        currConnPort = initialConnPort + 1;
-                        break;
-                    case "SCN02":
-                        currConnPort = initialConnPort + 2;
-                        break;
-                    case "SCN03":
-                        currConnPort = initialConnPort + 3;
-                        break;
-                    case "SCN04":
-                        currConnPort = initialConnPort + 4;
-                        break;
-                    case "SCN05":
-                        currConnPort = initialConnPort + 5;
-                        break;
-                    case "SCN06":
-                        currConnPort = initialConnPort + 6;
-                        break;
-                    case "SCN07":
-                        currConnPort = initialConnPort + 7;
-                        break;
-                    case "SCN08":
-                        currConnPort = initialConnPort + 8;
-                        break;
-                    case "SCN09":
-                        currConnPort = initialConnPort + 9;
-                        break;
-                    case "SCN10":
-                        currConnPort = initialConnPort + 10;
-                        break;
-                    case "RELAX":
-                        currConnPort = initialConnPort + 11;
-                        break;
-                    default:
-                        currConnPort = initialConnPort;
-                        break;
-                }
-            }
-            else if (cmd.Equals("PREVS"))
-            {
-                Console.WriteLine(currConnPort);
-                Connect(hololensIP, currConnPort, scene, cmd);
-                currConnPort = currConnPort - 1;
-                Console.WriteLine(currConnPort);
-            }
-            else if (cmd.Equals("NEXXT"))
-            {
-                Console.WriteLine(currConnPort);
-                Connect(hololensIP, currConnPort, scene, cmd);
-                currConnPort = currConnPort + 1;
-                Console.WriteLine(currConnPort);
-            }
-            else if (cmd.Equals("PLAYY"))
-            {
-                Console.WriteLine(currConnPort);
-                Connect(hololensIP, currConnPort, scene, cmd);
-            }
-            else if (cmd.Equals("STOPP"))
-            {
-                Console.WriteLine(currConnPort);
-                Connect(hololensIP, currConnPort, scene, cmd);
-            }
-            else if (cmd.Equals("PAUSE"))
-            {
-                Console.WriteLine(currConnPort);
-                Connect(hololensIP, currConnPort, scene, cmd);
-            }
-            else
-            {
-                switch (scene)
-                {
-                    case "SCN01":
-                        Connect(hololensIP, 9091, scene, cmd);
-                        currConnPort = 9091;
-                        break;
-                    case "SCN02":
-                        Connect(hololensIP, 9092, value, cmd);
-                        currConnPort = 9092;
-                        break;
-                    case "SCN03":
-                        Connect(hololensIP, 9093, value, cmd);
-                        break;
-                    case "SCN04":
-                        Connect(hololensIP, 9094, value, cmd);
-                        break;
-                    case "SCN05":
-                        Connect(hololensIP, 9095, value, cmd);
-                        break;
-                    case "SCN06":
-                        Connect(hololensIP, 9096, value, cmd);
-                        break;
-                    case "SCN07":
-                        Connect(hololensIP, 9097, value, cmd);
-                        break;
-                    case "SCN08":
-                        Connect(hololensIP, 9098, value, cmd);
-                        break;
-                    case "SCN09":
-                        Connect(hololensIP, 9099, value, cmd);
-                        break;
-                    case "SCN10":
-                        Connect(hololensIP, 9100, value, cmd);
-                        break;
-                    case "RELAX":
-                        Connect(hololensIP, 9101, value, cmd);
-                        break;
-                    default:
-                        Connect(hololensIP, 9090, value, cmd);
-                        break;
-                }
-            }
+            */
+            //Connect(hololensIP, initialConnPort, value);
         }
 
         // Start Sharing Service
@@ -246,20 +116,19 @@ namespace WebSocketTest
             Console.WriteLine("New Session Connected");
         }
 
-        
-        static void Connect(String[] holoIP, int serverPort, String value, String cmd)
+        static void Connect(String[] holoIP, int serverPort, String cmd)
         {
             int n = holoIP.Length;
             for (int i = 0; i < n; i++) {
                 Console.WriteLine(holoIP[i]);
                 Console.WriteLine("Connecting to Hololens - " + holoIP[i]) ;
-                TcpConnect(holoIP[i], serverPort, value, cmd);
+                TcpConnect(holoIP[i], serverPort, cmd);
                 Thread.Sleep(1000);
             }
         }
         
         // TCP Connection Code Snippet
-        static void TcpConnect(String server, int serverPort, String message, String cmd)
+        static void TcpConnect(String server, int serverPort, String cmd)
         {
             try
             {
@@ -271,7 +140,7 @@ namespace WebSocketTest
                 TcpClient client = new TcpClient(server, port);
 
                 // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message + " " + cmd);
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(cmd);
 
                 // Get a client stream for reading and writing.
                 //  Stream stream = client.GetStream();
@@ -281,7 +150,7 @@ namespace WebSocketTest
                 // Send the message to the connected TcpServer. 
                 stream.Write(data, 0, data.Length);
 
-                Console.WriteLine("Sent: {0}", message + " " + cmd);
+                Console.WriteLine("Sent: {0}", cmd);
 
                 // Receive the TcpServer.response.
 
